6379a0df11bb1c2ce18148eb20fd75f8e5270692
Theo <siwrist@yahoo.gr>
2019-11-12 00:50:10 +0100

    fixed to ns since UNIX epoch

--- back-end/utilities/utilities.js
+++ back-end/utilities/utilities.js
@@ -1,8 +1,7 @@
 module.exports = {
     generateUniqueId: function () {
-    //    can be replaced with
-    //    read all file titles -> cast to number -> increment -> return
-    var hrTime = process.hrtime();
-    return hrTime[0] * 1000000000 + hrTime[1];
-}
+        //    can be replaced with
+        //    read all file titles -> cast to number -> increment -> return
+        return Date.now();
+    }
 };
